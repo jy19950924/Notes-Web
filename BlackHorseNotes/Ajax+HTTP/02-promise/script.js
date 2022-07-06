7d3497954be74d33ab9c8b6aed007a2a6592a445
jy19950924 <350965504@qq.com>
2022-07-07 00:16:31 +0800

    promise of ajax

---
+++ BlackHorseNotes/Ajax+HTTP/02-promise/script.js
@@ -0,0 +1,45 @@
+
+const bookUrl = 'http://www.liulongbin.top:3006';
+
+function getBooks() {
+    // function () {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `${bookUrl}/api/getbooks`);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve.call(undefined, xhr.responseText);
+            } else {
+                reject.call(undefined, xhr.responseText);
+            }
+        }
+        xhr.send();
+    });
+}
+
+function addBook(formdate) {
+    return new Promise((resolve, reject) => {
+        let fd = new FormData(formdate)
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', `${bookUrl}/api/addbook`);
+        xhr.send(fd);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(JSON.parse(xhr.responseText));
+            }
+        }
+    });
+}
+let bookList = getBooks();
+bookList.then((response) => {
+    let booksList = response.data;
+    // let table = document.querySelector('table');
+
+    console.log(booksList);
+
+}).catch((error) => {
+
+    console.log("error:" + error);
+});
