44f2c029f180839b94cc792107ed5a93d3f1b59f
coderSheep <64951456+jy19950924@users.noreply.github.com>
2022-07-07 22:18:20 +0800

    use ajax for Promise by javascript

--- BlackHorseNotes/Ajax+HTTP/02-promise/script.js
+++ BlackHorseNotes/Ajax+HTTP/02-promise/script.js
@@ -2,44 +2,74 @@
 const bookUrl = 'http://www.liulongbin.top:3006';
 
 function getBooks() {
-    // function () {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', `${bookUrl}/api/getbooks`);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                resolve.call(undefined, xhr.responseText);
-            } else {
-                reject.call(undefined, xhr.responseText);
+        xhr.onreadystatechange = function (responseTe) {
+
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(JSON.parse(xhr.responseText));
+                }
             }
         }
         xhr.send();
     });
 }
-// https://juejin.cn/post/6968796449618264072
+
 function addBook(formdate) {
     return new Promise((resolve, reject) => {
+
         let fd = new FormData(formdate)
+        let fd1 = Object.fromEntries(fd.entries()) 
+
         let xhr = new XMLHttpRequest();
-        xhr.open('POST', `${bookUrl}/api/addbook`);
-        xhr.send(fd);
+        // xhr.open('POST', `${bookUrl}/api/addbook`);
+        xhr.open('POST', 'http://127.0.0.1:5000/test') 
+        xhr.send(JSON.stringify(fd1))
+
         xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                resolve(JSON.parse(xhr.responseText));
-            } else {
-                reject(JSON.parse(xhr.responseText));
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(JSON.parse(xhr.responseText));
+                }
+            } else if (xhr.readyState === 2) {
+                console.log('xhr', xhr.getAllResponseHeaders())
             }
         }
     });
 }
-let bookList = getBooks();
-bookList.then((response) => {
+function drawTable(booksList) {
+    let table = document.querySelector('table');
+    booksList.forEach(book => {
+        let newRow = table.insertRow();
+        Object.values(book).forEach((value) => {
+            let newCell = newRow.insertCell();
+            newCell.innerText = value;
+        })
+    });
+}
+
+getBooks().then((response) => {
     let booksList = response.data;
-    // let table = document.querySelector('table');
-
-    console.log(booksList);
-
+    drawTable(booksList);
 }).catch((error) => {
-
     console.log("error:" + error);
 });
+
+let submit = document.querySelector('button');
+submit.addEventListener('click', function () {
+    let addBookInfo = document.querySelector('form');
+    addBook(addBookInfo).then(() => {
+        getBooks().then((response) => {
+            let booksList = response.data;
+            drawTable(booksList);
+        })
+    }).catch((error) => {
+        console.log('error', error);
+    })
+}, false)
